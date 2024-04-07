9bd86392407c6526fed6a33e3a42c9bbd12d7659
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-07 11:11:30 +0530

    image slider

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+const prev = document.querySelector(".control_prev");
+const next = document.querySelector(".control_next");
+let index = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = "none";
+    }
+
+    imgs[index].style.display = "block";
+}
+
+changeSlide();
+
+prev.addEventListener("click", (e) => {
+    if (index > 0) {
+        index--;
+    } else {
+        index = imgs.length - 1;
+    }
+
+    changeSlide();
+});
+
+next.addEventListener("click", (e) => {
+    if (index < imgs.length - 1) {
+        index++;
+    } else {
+        index = 0;
+    }
+
+    changeSlide();
+});
